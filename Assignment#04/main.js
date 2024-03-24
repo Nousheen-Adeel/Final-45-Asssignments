f112ffe70441cbff7e32da7429aae7a708e34ddd
NousheenAdeel <nousheenfatimaadeel@gmail.com>
2024-03-25 00:41:47 +0500

    4th asssignment added

---
+++ Assignment#04/main.js
@@ -0,0 +1,4 @@
+"use strict";
+//Find a quote from a famous person you admire. Print the quote and the name of its author.//
+let famousQuote = `Hazrat Ali once said, "Let go of anything that brings you stress and sorrow."`;
+console.log(famousQuote);
