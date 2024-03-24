4b889b89d86e22adc685bd9abde749e612d59473
NousheenAdeel <nousheenfatimaadeel@gmail.com>
2024-03-25 01:44:26 +0500

    added assignment#6

---
+++ Assignment#06/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//storing a person's name in a variable with white spaces in begining and at the end//
+let personName = "    Nousheen   ";
+console.log(personName);
+console.log(personName.trim());
+//now printing a name using \t  //
+let fatherName = "\tMuhammed Anwer Pasha\t";
+console.log(fatherName);
+//now printing a name using \n //
+let motherName = "\nMubeen Fatima\n";
+console.log(motherName);
