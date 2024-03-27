9cacb2161c7e8fadcb5bb25623aa434d84189e46
NousheenAdeel <nousheenfatimaadeel@gmail.com>
2024-03-27 13:37:16 +0500

    Assignment#7 added

---
+++ Assignment#07/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// print number 8 in result doing addition, subtraction, multiplication and division//
+let additionResult = 6 + 2;
+console.log(additionResult);
+let subtractResult = 10 - 2;
+console.log(subtractResult);
+let multiplicationResult = 4 * 2;
+console.log(multiplicationResult);
+let divisionResult = 16 / 2;
+console.log(divisionResult);
