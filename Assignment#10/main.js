8aaea3529627d5965e6b248b556ae05d7b8b2337
NousheenAdeel <nousheenfatimaadeel@gmail.com>
2024-03-27 15:06:26 +0500

    Assignment#10 added

---
+++ Assignment#10/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+If you don’t have anything specific to write because your programs are too simple at this point,
+just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.*/
+//Saving my full name in a variable//
+let fullName = "Nousheen Fatima Adeel";
+//saving today's date in a variable//
+let todaysDate = "27 March 2023";
+//Printing name and date//
+console.log(`My full name is ${fullName} and I am doing this projhect on ${todaysDate}`);
