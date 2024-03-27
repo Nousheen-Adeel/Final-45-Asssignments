1d6a4e07738984c6b8e17943806ecd92afed9edf
NousheenAdeel <nousheenfatimaadeel@gmail.com>
2024-03-27 14:48:54 +0500

    Assignment#09 added

---
+++ Assignment#09/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/*Favorite Number: Store your favorite number in a variable.
+ Then, using that variable, create a message that reveals your favorite number. Print that message.*/
+let myfavoritenumber = 6;
+let message = `My favorite number is: ${myfavoritenumber}`;
+console.log(message);
