f0260b3b625da24d6f8816eccbb4ff4e54e75cd2
NousheenAdeel <nousheenfatimaadeel@gmail.com>
2024-03-28 18:16:44 +0500

    Assignment#11 added

---
+++ Assignment#11/main.js
@@ -0,0 +1,9 @@
+//Assignment#11//
+/*Names: Store the names of a few of your friends in a array called names.
+Print each person’s name by accessing each element in the list, one at a time.*/
+//Array//
+var names = ["Nusrat", "Mehreen", "Surraiya", "Mehwish", "Nadia"];
+// priniting Friends name using For loop//
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
