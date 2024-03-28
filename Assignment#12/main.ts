3daba97b4506a81e900e03cbebe9aa988d8772de
NousheenAdeel <nousheenfatimaadeel@gmail.com>
2024-03-28 18:31:30 +0500

    Assignment#12 Added

---
+++ Assignment#12/main.ts
@@ -0,0 +1,19 @@
+//Assignment#12//
+
+/*Greetings: Start with the array you used in Exercise 11, 
+but instead of just printing each person’s name, print a message to them.
+The text of each message should be the same,
+ but each message should be personalized with the person’s name.*/
+
+//Making an Array//
+let names:string[]= ["Nusrat", "Mehreen", "Surraiya","Mehwish", "Nadia"]
+
+
+ //Printing Friends name usine for each method//
+ names.forEach(friends => {
+     console.log(friends + " " + "May Allah Bless You Always" )
+ });
+
+
+
+
