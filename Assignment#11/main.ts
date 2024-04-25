e62b908da8ddb5a759a964720573747300945046
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-25 10:44:49 +0500

    Assignment#11 and 12 added

---
+++ Assignment#11/main.ts
@@ -0,0 +1,16 @@
+//Assignment#11//
+
+/*Names: Store the names of a few of your friends in a array called names. 
+Print each person’s name by accessing each element in the list, one at a time.*/
+
+//Array//
+
+let names:string[] = ["Nusrat", "Mehreen", "Surraiya", "Mehwish","Nadia"];
+
+// priniting Friends name using For loop//
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+
