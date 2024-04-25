f052310b5374b136f89e423f083e09e8fa09a765
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-25 10:27:32 +0500

    Assignment#06 added

---
+++ Assignment#06/main.js
@@ -0,0 +1,11 @@
+//storing a person's name in a variable with white spaces in begining and at the end//
+var personName = "    Nousheen   ";
+console.log(personName);
+//printing name after trimming
+console.log(personName.trim());
+//now printing a name using \t for creating space befor name//
+var fatherName = "\tMuhammed Anwer Pasha\t";
+console.log(fatherName);
+//now printing a name using \n for printing name in next line//
+var motherName = "\nMubeen Fatima\n";
+console.log(motherName);
---
+++ Assignment#06/main.ts
@@ -0,0 +1,15 @@
+//storing a person's name in a variable with white spaces in begining and at the end//
+
+let personName:string = "    Nousheen   "
+console.log(personName);
+
+//printing name after trimming
+console.log(personName.trim());
+
+//now printing a name using \t for creating space befor name//
+let fatherName: string =  "\tMuhammed Anwer Pasha\t"
+console.log(fatherName);
+
+//now printing a name using \n for printing name in next line//
+let motherName: string = "\nMubeen Fatima\n"
+console.log(motherName);
