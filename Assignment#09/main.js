815ea4a852632f1c44b9a3338cb7a2b8559e5c8e
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-25 10:36:41 +0500

    Assignment#09 added

---
+++ Assignment#09/main.js
@@ -0,0 +1,5 @@
+/*Favorite Number: Store your favorite number in a variable.
+ Then, using that variable, create a message that reveals your favorite number. Print that message.*/
+var myfavoritenumber = 6;
+var message = "My favorite number is: ".concat(myfavoritenumber);
+console.log(message);
---
+++ Assignment#09/main.ts
@@ -0,0 +1,9 @@
+/*Favorite Number: Store your favorite number in a variable.
+ Then, using that variable, create a message that reveals your favorite number. Print that message.*/
+
+ let myfavoritenumber:number = 6
+ let message:string = `My favorite number is: ${myfavoritenumber}`
+ 
+ console.log(message);
+
+ 
