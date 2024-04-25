263dc862f85b62af2123b8919fa8df85e4384fc6
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-25 10:39:06 +0500

    Assignment#10 added

---
+++ Assignment#10/main.js
@@ -0,0 +1,10 @@
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+If you don’t have anything specific to write because your programs are too simple at this point,
+just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.*/
+//Saving my full name in a variable//
+var fullName = "Nousheen Fatima Adeel";
+//saving today's date in a variable//
+var todaysDate = "27 March 2023";
+//Printing name and date//
+console.log("My full name is ".concat(fullName, " and I am doing this projhect on ").concat(todaysDate));
---
+++ Assignment#10/main.ts
@@ -0,0 +1,13 @@
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+If you don’t have anything specific to write because your programs are too simple at this point, 
+just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.*/
+
+//Saving my full name in a variable//
+let fullName:string = "Nousheen Fatima Adeel"
+
+//saving today's date in a variable//
+let todaysDate:string = "27 March 2023"
+
+//Printing name and date//
+console.log(`My full name is ${fullName} and I am doing this projhect on ${todaysDate}`);
