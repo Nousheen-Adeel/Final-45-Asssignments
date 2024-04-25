6078e09893fef228541536bb5651ed3933239728
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-25 10:29:51 +0500

    Assignment#07 added

---
+++ Assignment#07/main.js
@@ -0,0 +1,9 @@
+// print number 8 in result doing addition, subtraction, multiplication and division//
+var additionResult = 6 + 2;
+console.log(additionResult);
+var subtractResult = 10 - 2;
+console.log(subtractResult);
+var multiplicationResult = 4 * 2;
+console.log(multiplicationResult);
+var divisionResult = 16 / 2;
+console.log(divisionResult);
---
+++ Assignment#07/main.ts
@@ -0,0 +1,13 @@
+// print number 8 in result doing addition, subtraction, multiplication and division//
+
+let additionResult:number = 6 +2
+ console.log(additionResult);
+
+let subtractResult:number = 10-2;
+console.log(subtractResult);
+
+let multiplicationResult:number = 4*2;
+console.log(multiplicationResult);
+
+let divisionResult:number = 16/2;
+console.log(divisionResult);
