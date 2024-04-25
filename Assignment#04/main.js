a6fb656a137fbb037f518344a19a1afcd63095d6
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-25 10:18:13 +0500

    Assignmnet#04 added

---
+++ Assignment#04/main.js
@@ -0,0 +1,3 @@
+//Find a quote from a famous person you admire. Print the quote and the name of its author.//
+var famousQuote = "Hazrat Ali once said, \"Let go of anything that brings you stress and sorrow.\"";
+console.log(famousQuote);
---
+++ Assignment#04/main.ts
@@ -0,0 +1,5 @@
+//Find a quote from a famous person you admire. Print the quote and the name of its author.//
+
+let famousQuote: string = `Hazrat Ali once said, "Let go of anything that brings you stress and sorrow."`
+
+console.log(famousQuote);
