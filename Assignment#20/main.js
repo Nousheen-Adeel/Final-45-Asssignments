6f712874c8338d76423ba24a810fe3d51b5b71dc
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-05-01 08:25:42 +0500

    Assignment#20 added

---
+++ Assignment#20/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Assignment#20
+/*Think of something you could store in a array.
+ For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+ Write a program that creates a list containing these items.*/
+//Making as array
+let shapes = ["Circle", "Triangle", "Rectangle", "Squrae", "Heart",];
+console.log("****list of shapes****");
+shapes.forEach(Array => {
+    console.log(Array);
+});
