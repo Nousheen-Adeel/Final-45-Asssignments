b696f0f69a71f21d68d04a5b6986f9cf32bf8186
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-05-01 09:04:29 +0500

    Assignment#23 added

---
+++ Assignment#23/main.js
@@ -0,0 +1,51 @@
+"use strict";
+//Assignment#23
+/*Conditional Tests: Write a series of conditional tests.
+ Print a statement describing each test and your prediction for the results of each test.
+ Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests.
+ Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+let weather1 = "sunny";
+let weather2 = "cloudy";
+let weather3 = "rainy";
+let weather4 = "snowy";
+let weather5 = "windy";
+//Test1
+console.log("Is weather1 =='sunny'? I predict True.");
+console.log(weather1 == "sunny");
+//Test2
+console.log("Is weather2 == 'cloudy? I predict true.");
+console.log(weather2 == "cloudy");
+//Test3
+console.log("Is weather3 == 'rainy'? I predict true.");
+console.log(weather3 == "rainy");
+//Test4
+console.log("Is weather4 =='snowy'? I predict true.");
+console.log(weather4 == "snowy");
+//Test5
+console.log("Is weather5 == 'windy'? I predict true.");
+console.log(weather5 == "windy");
+//Test6
+console.log("Is weather1 =='sunny'? I predict false.");
+console.log(weather1 != "sunny");
+//Test7
+console.log("Is weather2 == 'cloudy'? I predict false.");
+console.log(weather2 != "cloudy");
+//Test8
+console.log("Is weather3 == 'rainy'? I predict false.");
+console.log(weather3 != "rainy");
+//Test9
+console.log("Is weather4 =='snowy'? I predict false.");
+console.log(weather4 != "snowy");
+//Test10
+console.log("Is weather5 == 'windy'? I predict false.");
+console.log(weather5 != "windy");
