7c4639eaae4973fa770bc3ba76c27b560a8eee57
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-05-01 08:40:33 +0500

    Assignment#21 added

---
+++ Assignment#21/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/*Then think of something you could store in a TypeScript Object.
+ Write a program that creates Objects containing these items.*/
+//Making an object, 
+let personadata = {
+    name: "Nousheen",
+    age: 36,
+    gender: "Female",
+    education: "Masters in Applied mathematics"
+};
+console.log(personadata);
---
+++ Assignment#21/main.ts
@@ -0,0 +1,15 @@
+//Assignment#21
+/*Then think of something you could store in a TypeScript Object.
+ Write a program that creates Objects containing these items.*/
+
+ //Making an object, 
+
+ let personadata = {
+    name: "Nousheen",
+    age: 36,
+    gender:"Female",
+    education:"Masters in Applied mathematics"
+
+ }
+
+ console.log(personadata);
