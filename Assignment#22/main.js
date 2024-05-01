b17f57ca3cde35ae129ebe039020885871931656
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-05-01 08:48:18 +0500

    Assignment#22 added

---
+++ Assignment#22/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Assignment#22
+/*Intentional Error: If you haven’t received an array index error in one of your programs yet,
+ try to make one happen. Change an index in one of your programs to produce an index error.
+ Make sure you correct the error before closing the program.*/
+//making an array 
+let colors = ["pink", "red", "yellow", "green"];
+console.log(colors[10]);
+//above thing will give an intentional array as index 10 was not there
+//making it correct  now
+console.log(colors[3]);
