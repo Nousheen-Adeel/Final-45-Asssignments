d944301782c344795258ae927d0b98c62e2d332c
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-05-11 07:43:03 +0500

    Assignment#36

---
+++ Assignment#36/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Assignment#36
+/*T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+ The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.*/
+//making function
+function make_shirt(size, message) {
+    console.log(`Your Shirt size is ${size} and it has a print ${message} on it`);
+}
+make_shirt(32, "You are Brave");
+make_shirt(34, "jhhg");
---
+++ Assignment#36/main.ts
@@ -0,0 +1,17 @@
+//Assignment#36
+/*T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+ The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.*/
+
+ //making function
+
+ function make_shirt(size: number, message: string){
+    console.log (`Your Shirt size is ${size} and it has a print ${message} on it`)
+}
+
+
+make_shirt(32,"You are Brave");
+
+
+
+
+
