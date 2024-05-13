f4716d974a7569e2ff69e6efba47d697a0416b97
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-05-13 15:06:14 +0500

    Assignment#41 added

---
+++ Assignment#41/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//Assignment#41
+/*Magicians: Make a array of magician’s names.
+Pass the array to a function called show_magicians(), which prints the name of each magician in the array.*/
+//making as array
+let magician_names = ["Harry Potter", "Alice", "John"];
+//making function
+function show_magicians(magicians) {
+    magicians.forEach(names => console.log(names));
+}
+//calling the function
+show_magicians(magician_names);
