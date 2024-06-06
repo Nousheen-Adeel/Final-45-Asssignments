fd888cd6afa077014abc527be6119d26990b204b
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-06-06 21:04:19 +0500

    assignment#45 added

---
+++ Assignment#45/main.ts
@@ -0,0 +1,20 @@
+// // //Assignment#45
+
+// // /*Cars: Write a function that stores information about a car in a Object.
+// // The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments.
+// // Call the function with the required information and two other name-value pairs, such as a color or an optional feature.
+// // Print the Object that’s returned to make sure all the information was stored correctly.*/
+
+
+function car_info(manufacturer: string, model: string, ...options: [string, any][]) {
+  let make_car = {
+     manufacturer, 
+     model,
+    };
+
+  options.forEach(([key, value]) => (make_car as any)[key] = value);
+  return make_car;
+}
+
+console.log(car_info("Toyota", "Carolla", ["color", "silver"], ["year", 2022]));
+console.log(car_info("Ford", "Fiesta", ["color", "navy blue"], ["sunroof", true]));
